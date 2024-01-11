de837916351e2369e040c881a06eaab48d41e7d7
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-11 14:56:06 +0100

    about-us added in app component

--- unpack/src/app/app.component.ts
+++ unpack/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    HomeComponent, 
+    AboutUsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
