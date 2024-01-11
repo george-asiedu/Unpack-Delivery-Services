2c6bdb0433cdc4562240be4ab618a1f68bb762c4
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-11 08:45:54 +0100

    Roboto font added

--- unpack/tailwind.config.js
+++ unpack/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans-serif': ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
