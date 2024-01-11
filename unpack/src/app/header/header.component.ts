c2f5609cf77a2aa71093af115ae5073664e3b082
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-11 10:49:17 +0100

    update on header component

--- unpack/src/app/header/header.component.ts
+++ unpack/src/app/header/header.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMenuOpen = false;
 
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('clicked')
+  }
 }
