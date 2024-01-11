d88ea41c23758d628e89228645d19ed3ccca66dc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-11 09:05:39 +0100

    colors added

--- unpack/tailwind.config.js
+++ unpack/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans-serif': ['Roboto']
+      }, 
+      colors: {
+        heading: '#FF5E41',
+        black: '#222',
+        paragraph: '#A7A7A7',
+        smallHeading: '#7A7A7A',
+        ratings: '#FFC952',
+        borderColor: '#BDBDBD'
       }
     },
   },
